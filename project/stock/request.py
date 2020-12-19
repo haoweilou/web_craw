9972293d4c41c5f6dea42775b8a4b0222bb6640e
haoweilou <591193117@qq.com>
2020-12-20 02:08:12 +1100

    stock add more efficiency with lower bug rate

--- project/stock/request.py
+++ project/stock/request.py
@@ -1,7 +1,9 @@
+import re
+from numpy.core.numeric import outer
 import requests
 import json
 from datetime import datetime, timedelta
-
+from multiprocessing.dummy import Pool
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 }
@@ -10,6 +12,8 @@ class StockSubscriber():
     def __init__(self):
         self.sections = ["Materials","SS","Energy","DF","PBLS","HCES","RE","ME","Retailing","FBT","CG","CPS","THE","Utilities","TS"]
         self.sections += ["CS","Transportation","Banks","CDA","Insurance","AC","FSR","HPP","SSE","NA"]
+        self.cache = []
+        self.incorrec = []
 
     def gedPriceForSection(self,section):
         fd = open(f"codes/{section}.json","r")
@@ -45,9 +49,11 @@ class StockSubscriber():
     def getPrice(self,code):
         price_url = "https://api.61financial.com.au/marketdata/latest/?code="+code
         try:
-            return float(requests.get(url=price_url,headers=headers).json()[-1]['last'])
-        except KeyError:
-            return 0.0
+            number = requests.get(url=price_url,headers=headers).json()
+            return float(number[-1]['last'])
+        except:
+            self.incorrec.append(code)
+            return -1
 
     def getAllCodes(self):
         f = open("codes/ALL.json","r")
@@ -72,6 +78,38 @@ class StockSubscriber():
         for code in data['data']:
             output.append(code['code'])
         return output
+        
+    def getPrices(self,code):
+        print(code,end=" ")
+        price = self.getPrice(code)
+        self.cache.append({"code":code,"price":price})
+    
+    def multithreadUpdate(self,section):
+        codes = self.getCodeForSection(section)
+        #codes_1 = codes[:len(codes)//2]
+        #codes_2 = codes[len(codes)//2:]
+        print(len(codes))
+        pool = Pool(3)
+        pool.map(self.getPrices,codes)
+        print("")
+        while(self.incorrec!=[] and len(self.incorrec) > 6):
+            print(f"{len(self.incorrec)} exists")
+            codes = self.incorrec.copy()
+            self.incorrec = []
+            pool = Pool(3)
+            pool.map(self.getPrices,codes)
+            print("")
+        self.incorrec = []
+
+        database = json.load(open("database.json","r"))
+        for item in self.cache:
+            code = item['code']
+            price = item['price']
+            database[code].append(price)
+
+        with open("database.json","w") as f:
+            f.write(json.dumps(database,indent=4))
+        self.cache = []
 
     def update(self,section):
         codes = self.getCodeForSection(section)
@@ -91,4 +129,6 @@ if __name__ == '__main__':
     a = StockSubscriber()
     #output = a.gedPriceForSection("RE")
     a.initialise()
-    a.update("ALL")
+    now = datetime.now()
+    a.multithreadUpdate("ALL")
+    print(datetime.now()-now)
